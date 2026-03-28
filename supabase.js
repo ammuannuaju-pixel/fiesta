294295965e0cf8c4098467d66dc82caac982a1f2
ammuannuaju-pixel <ammuannuaju@gmail.com>
2026-03-28 10:40:29 +0530

    fix supabase

--- supabase.js
+++ supabase.js
@@ -1,5 +1,7 @@
 var currentUser = null;
-var currentProfile = null;upabase client initialization
+var currentProfile = null;
+
+// Supabase client initialization
 
 const SUPABASE_URL      = "https://cfnhtevtdgintewvofhc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmbmh0ZXZ0ZGdpbnRld3ZvZmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4ODQzMTIsImV4cCI6MjA4ODQ2MDMxMn0.LEIiCqimSN5EzJ50pYIKfA6Zw6o3p-G_g8l6QRBPV2c";
